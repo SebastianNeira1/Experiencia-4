e23fdcb1df5b63e146cac22f7f95ea87b1843f61
Francisco <fran.peagonza@gmail.com>
2016-12-20 15:14:56 -0300

    base de datos

--- application/views/footer.php
+++
@@ -1,8 +0,0 @@
-
-<script src="<?php echo base_url('bower_components/jquery/dist/jquery.min.js') ?>" ></script>
-<script src="<?php echo base_url('bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
-<script type="text/javascript" src="<?php  echo base_url('js/codigo.js') ?>"></script>
-
-
-</body>
-</html>
